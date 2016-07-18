79c4794cd44ce72d558f43984cf08b03373c6f92
davidkc0 <dvid1994@gmail.com>
2016-07-18 16:38:00 -0700

    Initial Commit

---
+++ imageFlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imageFlow
+//
+//  Created by David Ciaffoni on 7/18/16.
+//  Copyright © 2016 David Ciaffoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
